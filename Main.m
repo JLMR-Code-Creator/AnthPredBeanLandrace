349a7730da49e497b0b3b7b88acd871c99a1a643
JOSE LUIS MORALES <jluismorey@hotmail.com>
2024-08-06 10:33:49 -0600

    adding code

--- Main.m
+++ Main.m
@@ -38,31 +38,8 @@ Plot_CNN_1H_HSI_LAB('../Images/Pob_frijol_27mm/NeuroEvolution/Final46/CNNNeuroEx
 
 Plot_CNN_3H_HSI_LAB('../Images/Pob_frijol_27mm/NeuroEvolution/Final46/CNNNeuroExp1/', 'OutcomeLAB3H2D.mat',  'OutcomeHSI3H2D.mat');
 
-% fue de prueba para verificar funcionamiento PlotCNN_LAB('../Images/Pob_frijol_27mm/NeuroEvolution/Final46/CNNNeuroExp1/', 'HomoCNNADAM250_a.mat', 3)
+% Show a especific result of segmentation
 
-% Creation of structures as of mask
-%DivMorphoMask('src/Images/', 'src/Images/DB_StructMask/', 40);
-%Binary images or masks to data set
-%MorphoMask2DB('src/Images/', 'src/Images/DB_StructMask/', '*.mat', 'DataCenter');
-%Dataset to leaning aprouches
-
-% Average and Regression
-%  val = Morpho_Regression('src/Images/DB_StructMask/DataCenter/', '*.mat', 1, 1, 0,'Report40AVG_Reg', '','','','');
-
-% PCA and Regression
-%   vec = 3:30;
-%   val = Morpho_MultiRegression('src/Images/DB_StructMask/DataCenter/', '*.mat', vec, 1, 2, 'Report402D_Regress', '', '', '', '');
-
-
-% Average  and ANN
-%    val = Morpho_ANN('src/Images/DB_StructMask/DataCenter/', '*.mat', 1, 1, 0, 0, 'Report40AVG_ANN', '', '', '', '');
-
-
-% PCA and ANN
-%    vec = 3:30;
-%    val = Morpho_MultiANN('src/Images/DB_StructMask/DataCenter/', '*.mat', vec, 1, 2, 'Report402D_ANN', '', '', '', '');
-
-%PMF and AntEstNet 
-%   Morpho_ProcessCNN('src/Images/DB_StructMask/DataCenter/', '*.mat', 500, 'Report_40P_CNN_500')
+val = Impl_ShowEspecificSegmentationResult('../Images/Pob_frijol_27mm/','*.tif', 'P-34B');
 
 
