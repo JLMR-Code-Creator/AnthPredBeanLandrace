2892bc49c35fa78204f07c4baa8e189778559daa
JLMR-Code-Creator <jluismorey@hotmail.com>
2025-03-05 16:03:28 -0600

    código

--- src/LandracesClassification/FiltraDBKNowledge.m
+++ src/LandracesClassification/FiltraDBKNowledge.m
@@ -1,5 +1,5 @@
 function FiltraDBKNowledge(pathDB, nameDataSet)
-
+% FiltraDBKNowledge('../Images/DB/', 'db5knn.mat')
    rootdir = pathDB;
    filename1 = strcat(rootdir,filesep, nameDataSet);
    db = load(filename1,'-mat');
@@ -8,13 +8,18 @@ function FiltraDBKNowledge(pathDB, nameDataSet)
    train_median_lab = db.train_median_lab;
    train_ab = db.train_1Hab;
 
-   devstd = std(train_ab);
-   idx = devstd > 0;
-   datos = train_ab(:,idx); 
-    
-   clase = string(db.clase);
+   devstd = std(train_lab);
+   idx = find(devstd > 0);
+   datos = train_lab(:,idx); 
+ %  
+ %  maxi= max(train_ab);
+ %  idx = find(maxi > min(maxi));
+ %  datos = train_ab(:,idx); 
+ %
 
-   save('frijoldb.mat', "clase","datos");
+   clase = string(db.clase);
+   outputFile = strcat(pathDB, 'frijoldbLAB.mat'); 
+   save(outputFile, "clase","datos");
 
 
 end
