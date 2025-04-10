ffdfa2a78d909d07530f241c9d65312d40c129a5
JLMR-Code-Creator <jluismorey@hotmail.com>
2025-04-10 17:49:13 -0600

    more code

--- src/LandracesClassification/ConcentraResultados.m
+++ src/LandracesClassification/ConcentraResultados.m
@@ -16,18 +16,48 @@ if(H_lch==0)
 end
 
 
-figure();
-labels = {'DeepGA CNN with \newline 3 PMF for CIE L*a*b* ', ...
-     'DeepGA CNN with \newline 3 PMF for CIE L*C*H* '};
- [p1,tbl,stats] =  anova1([listAccuracyLAB,listAccuracyLCH], labels);
- [c,m,h,nms]=multcompare(stats,'CType','hsd'); 
- [ncomp,nccol] = size(c);
-disp(' Comparación de grupos  - Mostrandos las diferencias significativas')
-for j=1:ncomp
-  if c(j,nccol) <= 0.05 
-     disp(['  Grupo ' labels{c(j,1)} ' de ' labels{c(j,2)} ' - p = ' num2str(c(j, nccol))]); 
-  end
+[h, p] = ttest2(listAccuracyLAB, listAccuracyLCH);
+
+% Mostrar resultados
+if h == 0
+    disp(['No hay diferencia significativa entre los modelos (p = ' num2str(p) ')']);
+else
+    disp(['Sí hay diferencia significativa entre los modelos (p = ' num2str(p) ')']);
 end
+
+figure();
+labels = {'DeepGA CNN con \newline 3 PMF en CIE L*a*b*', ...
+          'DeepGA CNN con \newline 3 PMF en CIE L*C*h*'};
+boxplot([listAccuracyLAB(:); listAccuracyLCH(:)], ...
+        [repmat({'LAB'}, 30, 1); repmat({'LCH'}, 30, 1)]);
+ylabel('Accuracy');
+
+
+
+meanLAB = mean(listAccuracyLAB);
+stdLAB = std(listAccuracyLAB);
+maxLAB = max(listAccuracyLAB);
+minLAB = min(listAccuracyLAB);
+
+meanLCH = mean(listAccuracyLCH);
+stdLCH = std(listAccuracyLCH);
+maxLCH = max(listAccuracyLCH);
+minLCH = min(listAccuracyLCH);
+
+cm = confusionchart(YTest, YPredTest)
+
+
+% labels = {'DeepGA CNN with \newline 3 PMF for CIE L*a*b* ', ...
+%     'DeepGA CNN with \newline 3 PMF for CIE L*C*H* '};
+% [p1,tbl,stats] =  anova1([listAccuracyLAB,listAccuracyLCH], labels);
+% [c,m,h,nms]=multcompare(stats,'CType','hsd');
+% [ncomp,nccol] = size(c);
+% disp(' Comparación de grupos  - Mostrandos las diferencias significativas')
+% for j=1:ncomp
+%     if c(j,nccol) <= 0.05
+%         disp(['  Grupo ' labels{c(j,1)} ' de ' labels{c(j,2)} ' - p = ' num2str(c(j, nccol))]);
+%     end
+% end
 
 
 
