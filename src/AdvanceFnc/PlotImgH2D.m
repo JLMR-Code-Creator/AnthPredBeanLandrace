102fb8b56788aa1afbef9f47a28e4bac27ebe8fa
JLMR-Code-Creator <74218508+JLMR-Code-Creator@users.noreply.github.com>
2026-07-13 00:03:07 -0600

    adding code

--- src/AdvanceFnc/PlotImgH2D.m
+++ src/AdvanceFnc/PlotImgH2D.m
@@ -3,18 +3,19 @@ data_list = [];
 % Process each selected image
 for i = 1:size(fileList, 1)
     img = imread( fileList.FullPath(i));
-    archivo = fileList.Name(i);             % Nombre del imagen
-    maskFile = strrep(archivo,'.tif','.mat');   % Nombre de la poblaci?n
+    archivo = fileList.Name(i);                          % Nombre del imagen
+    maskFile = strrep(archivo,'.tif','.mat');            % Nombre de la poblaci?n
     FullPathMask = strcat(fileList.Folder(i),'/Masks/');
-    L = load(strcat(FullPathMask, maskFile));       % Carga el archivo de la m?scara     
+    L = load(strcat(FullPathMask, maskFile));            % Carga el archivo de la m?scara     
     Mask = uint8(L.Mask);
     %Mask = ~Mask; 
     [MaskC] = CleanPixels(Mask, 1000);
     [I_Lab] = RGB2PCS(img, fileList.Folder(i), strcat('/',archivo));
-    [Lab_Values, data_raw]= ROILab(I_Lab, MaskC);
+    [Lab_Values, data_raw] = ROILab(I_Lab, MaskC);
     data_list = [data_list;Lab_Values];
     % Further processing can be added here
 end
     [cie_ab, cie_la, cie_lb, pixels] = Pixel2DABLALB(data_list);
     [cie_ch, cie_lc, cie_lh, c1] = Pixels2Hist2DCHLCLH(data_list);
-    mesh(cie_ch), xlabel("a"), ylabel("b")
+    cie_lh(cie_lh <= 0.00005) = 0;
+    figure(); mesh(cie_lh), xlabel("h°"), ylabel("L*")
